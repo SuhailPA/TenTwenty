001ca73e9cb6e6be3371a378c317c08d7d418522
SuhailPA <suhailabbas23@gmail.com>
2022-01-08 19:00:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/suhail/tentwenty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.suhail.tentwenty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
